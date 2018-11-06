ade606b2e060fd9c727ee5a4fa2cab9d13888b97
Simin Jie <simin_jie@163.com>
2018-11-06 21:06:11 +0800

    041-和为S的连续正数序列：重构

--- 041-和为S的连续正数序列/find_continous_sequence.py
+++ 041-和为S的连续正数序列/find_continous_sequence.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from __future__ import division
 import math
 
 
@@ -46,18 +47,23 @@ class Solution:
 
         算法3：
         假设目标序列的长度为 n：
-            * n 为奇数：序列中间的值是 xm，序列可以表示为 [... xm - 1, xm, xm + 1, ...]，xm 左右两边的数字一样多，并且可以相互抵消非 xm 部分，所以序列的平均值是 xm。
+            ① n 为奇数：序列中间的值是 xm，序列可以表示为 [... xm - 1, xm, xm + 1, ...]，xm 左右两边的数字一样多，并且可以相互抵消非 xm 部分，所以序列的平均值是 xm。
                 另外 xm == S / n，因为 S 可以被 n 整除，所以 S % n == 0
-            * n 为偶数：序列可以表示为 [... x(m) - 1, x(m), x(m+1), x(m+1) + 1...]，x(m)、x(m+1) 两边的数字一样多，并且可以相互抵消非 x(m)、x(m+1) 的部分，
+            ② n 为偶数：序列可以表示为 [... x(m) - 1, x(m), x(m+1), x(m+1) + 1...]，x(m)、x(m+1) 两边的数字一样多，并且可以相互抵消非 x(m)、x(m+1) 的部分，
                 所以平均值是 (x(m) + x(m+1)) / 2。
                 (xm + xm+1) / 2 = S / n，而 xm+1 = xm + 1，所以 (2xm + 1) / 2 = S / n => xm + 0.5 = S / n
-                所以 nxm + 0.5n = S，所以 S 整除 n = xm，然后余数 = S - xm = 0.5n，即 S % n = 0.5n。余数的定义为：被除数 - 除数 * 整除时的商。
-            * 第一个数为 S / n - (n - 1) / 2。证明：
-                    a1 + an = 2 * s / n - ①
-                    an = a1 + n - 1 - ②
+                所以 nxm + 0.5n = S，所以 S % n = 0.5n。余数的定义为：被除数 - 除数 * 整除时的商。
+            ③ 第一个数为 S / n - (n - 1) / 2。证明：
+                    a1 + an = 2 * s / n - ① -- 等差数列求和
+                    an = a1 + n - 1 - ② -- 首项和末项的关系
                     将 ② 代入 ①：2a1 + n - 1 = 2 * s / n，a1 = (2s / n + 1 -n) / 2 = s / n + (1 - n) / 2
 
-        n 的最大长度，即 n 从 1 开始，根据等差数列求和公式：S >= (1 + n) * n / 2，n <= 根号(2S)
+            ④ n 的最大长度。由于只能是正数，最小的正数为 1，第一个数最小时 n 最大。根据等差数列求和公式：S = (1 + 1 + n - 1) * n / 2
+            => 2S = n * (1 + n) => 2S > n^2 => n < 根号(2S) => n <= int(根号(2S))
+
+            根据④可以确认 n 的范围是 [2,  根号(2S))
+            根据①、②可以确认所有的 n
+            根据③可以由 n、S 计算出 a1 和 an，从而得到列表
 
         复杂度分析：
         时间复杂度：O(根号n)
@@ -66,10 +72,13 @@ class Solution:
         来自 https://www.nowcoder.com/questionTerminal/c451a3fd84b64cb19485dad758a55ebe
         """
         result_list = []
+        # Python2 的除法（/ 操作符）对于整数是整除，所以这里将其变为 float。牛客网的问题
+        # 导致 from __future__ import division 不能正常工作
+        tsum = float(tsum)
         n = int(math.sqrt(2 * tsum))
         while n >= 2:
             if (n % 2 == 1 and tsum % n == 0) or (n % 2 == 0 and tsum % n == 0.5 * n):
-                first_num = int(tsum / n - (n - 1) / 2)
+                first_num = int(tsum / n + (1 - n) / 2)
                 result = list(range(first_num, first_num + n))
                 result_list.append(result)
             n -= 1
