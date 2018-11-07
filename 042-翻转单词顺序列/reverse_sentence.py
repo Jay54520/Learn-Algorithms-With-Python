de0caa8ecb8b3dc3375ef76744b847b9af877478
Simin Jie <simin_jie@163.com>
2018-11-07 16:02:35 +0800

    042-翻转单词顺序列：修复题目错误

--- 042-翻转单词顺序列/reverse_sentence.py
+++ 042-翻转单词顺序列/reverse_sentence.py
@@ -3,7 +3,7 @@ class Solution:
     def ReverseSentence(self, s):
         """
         翻转句子中单词的顺序，但单词内字符的顺序不变。标点符号和普通字母一样处理。
-        例如输入“I am a student”，则输出 “student. a am I”。
+        例如输入“I am a student.”，则输出 “student. a am I”。
 
         算法1：
         我们认为句子是以空格分隔的多个字符，所以先以空格进行 split，再调用自带的 reversed 对其翻转，
