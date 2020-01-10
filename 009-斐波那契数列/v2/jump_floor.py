b0ea23d45991bad16767e77c0d12d6619d4930f9
Simin Jie <simin@16financial.com>
2020-01-10 11:09:58 +0800

    add jump_floor

---
+++ 009-斐波那契数列/v2/jump_floor.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    题目描述
+    一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+
+    1 floor: 1 way, jumps once
+    2 floors:
+    * jumps 1 floor, remaining 1 floor: f(1)
+    * jumps 2 floors, remaining 0 floor: f(0). We assume f(0) is 1 although there's no zero floor
+    3 floors:
+    * jumps 1 floor, remaining 2 floor: f(2)
+    * jumps 2 floors, remaining 1 floor: f(1).
+    So the formula is
+
+    1 (n <= 1)
+    f(n-1) + f(n-2)
+
+    We can see the formula is same with fib
+    """
+    def jumpFloor(self, number):
+        pass
