5f6df3e8057e611d96777d64e4236615f0077c08
Simin Jie <simin@16financial.com>
2020-01-13 15:40:33 +0800

    优化 020-顺时针打印矩阵/v1

--- 020-顺时针打印矩阵/v1/matrix.py
+++ 020-顺时针打印矩阵/v1/matrix.py
@@ -1,30 +1,14 @@
 # -*- coding:utf-8 -*-
-from collections import defaultdict
-
-RIGHT = 'right'
-LEFT = 'left'
-UP = 'up'
-DOWN = 'down'
-
-NEXT_DIRECTIONS = {
-    RIGHT: DOWN,
-    DOWN: LEFT,
-    LEFT: UP,
-    UP: RIGHT
-}
-
-
 class Solution:
 
     def __init__(self):
         """使实例用到的所有属性在 __init__ 中都能读到，可以提高可读性"""
-        self.matrix = []
+        self.row_start = 0
+        self.row_end = 0
+        self.col_start = 0
+        self.col_end = 0
         self.result = []
-        self.start_row = 0
-        self.end_row = 0
-        self.start_col = 0
-        self.end_col = 0
-        self.whole_cols = defaultdict(list)
+        self.matrix = []
 
     def printMatrix(self, matrix):
         """
@@ -51,7 +35,15 @@ class Solution:
         向左移动一个位置并向左读取，到边界后；向上移动一个位置并向上读取，到边界后；
         又向右移动一个位置并开始读取；然后又向右，这样一直循环。
 
-        终止条件：碰到边界，转向后还是边界。
+        终止条件：开始移动时碰到边界
+        [
+            [1]
+        ]
+        假设向右：row_start += 1, row_start = 1
+        假设向下：col_end -= 1, col_end = -1
+        向左：row_end -= 1, row_end = -1
+        向上： col_start += 1, col_start = 1
+        总结就是 row_start > row_end or col_start > col_end
 
         证明：
 
@@ -74,112 +66,44 @@ class Solution:
         * 没有抽取出读取行、列的函数，导致重复，使代码逻辑更复杂
 
         """
-        # 空矩阵
-        if not matrix:
-            return []
+        if not matrix or not matrix[0]:
+            return self.result
 
-        # 这些初始化放在 __init__ 中更好，但是题目已经固定了，就不好改了。
         self.matrix = matrix
-        self.init_original_cols()
-        self.result = []
-
-        # ------------通过开始、结束行、列的索引来划定边界------------
-        self.start_row = 0
-        self.end_row = len(matrix) - 1
-        self.start_col = 0
-        self.end_col = len(matrix[0]) - 1
-        # ------------通过开始、结束行、列的索引来划定边界------------
-
-        direction = RIGHT
-        current_row = 0
-        current_col = 0
-        self.move(current_row, current_col, direction)
+        self.row_end = len(matrix) - 1
+        self.col_end = len(matrix[0]) - 1
+        self.right()
         return self.result
 
-    def move(self, current_row, current_col, direction):
-        """
-        将当前行或列按照 direction 方向加入 result。然后试探能否向下一个方向移动，
-        如果不能，返回；否则，调整边界和当前坐标，将已读的行、列设置为新边界，当前坐标
-        是转向后的下一个元素。
-        """
-        if direction == RIGHT:
-            # 读取当前行
-            self.result.extend(self.get_row(current_row, direction))
-            # 如果是最后一行，那么就不能向下了
-            if current_row == self.end_row:
-                return
-            else:
-                # 将已读行设置为边界
-                self.start_row += 1
-                # ------- 当前元素变为下一行最后一个元素 ------------
-                current_row = self.start_row
-                current_col = self.end_col
-                # ------- 当前元素变为下一行最后一个元素 ------------
-                self.move(current_row, current_col, NEXT_DIRECTIONS[direction])
-        elif direction == DOWN:
-            # 读取当前列
-            self.result.extend(self.get_col(current_col, direction))
-            # 如果是第一列，那么就不能向左了
-            if current_col == self.start_col:
-                return
-            else:
-                # 将已读列设置为边界
-                self.end_col -= 1
-                # ------- 当前元素变为最后一行最后一列 ------------
-                current_row = self.end_row
-                current_col = self.end_col
-                # ------- 当前元素变为最后一行最后一列 ------------
-                self.move(current_row, current_col, NEXT_DIRECTIONS[direction])
-        elif direction == LEFT:
-            self.result.extend(self.get_row(current_row, direction))
-            # 如果当前行是第一行，那么不能向上了
-            if current_row == self.start_row:
-                return
-            else:
-                # 将已读行设置为边界
-                self.end_row -= 1
-                # ------- 当前元素变为最后一行第一个元素 ------------
-                current_row = self.end_row
-                current_col = self.start_col
-                # ------- 当前元素变为下一行最后一个元素 ------------
-                self.move(current_row, current_col, NEXT_DIRECTIONS[direction])
-        else:  # 向上
-            self.result.extend(self.get_col(current_col, direction))
-            # 如果当前列是最后一列，那么不能向右了
-            if current_col == self.end_col:
-                return
-            else:
-                # 将已读列设置为边界
-                self.start_col += 1
-                # ------- 当前元素变为第一行第一个元素 ------------
-                current_row = self.start_row
-                current_col = self.start_col
-                # ------- 当前元素变为第一行第一个元素 ------------
-                self.move(current_row, current_col, NEXT_DIRECTIONS[direction])
+    def right(self):
+        if self.out_of_border():
+            return
+        self.result.extend(self.matrix[self.row_start][self.col_start:self.col_end + 1])
+        self.row_start += 1
+        self.down()
 
-    def get_row(self, row, direction):
-        """按照 direction 返回第 row 行的元素（使用当前边界）"""
-        result = self.matrix[row][self.start_col:self.end_col + 1]
-        if direction == RIGHT:
-            return result
-        else:
-            return reversed(result)
+    def down(self):
+        if self.out_of_border():
+            return
+        for i in range(self.row_start, self.row_end + 1):
+            self.result.append(self.matrix[i][self.col_end])
+        self.col_end -= 1
+        self.left()
 
-    def get_col(self, col, direction):
-        """按照 direction 返回第 col 列的元素（使用当前边界）"""
-        whole_col = self.whole_cols[col]
-        result = whole_col[self.start_row: self.end_row + 1]
-        if direction == UP:
-            return reversed(result)
-        else:
-            return result
+    def left(self):
+        if self.out_of_border():
+            return
+        self.result.extend(reversed(self.matrix[self.row_end][self.col_start:self.col_end + 1]))
+        self.row_end -= 1
+        self.up()
 
-    def init_original_cols(self):
-        """初始化原始矩阵的列，避免之后重复计算"""
-        if not self.matrix:
-            raise ValueError('没有初始化原始矩阵')
-        col_num = len(self.matrix[0])
-        for row in self.matrix:
-            for col in range(col_num):
-                self.whole_cols[col].append(row[col])
+    def up(self):
+        if self.out_of_border():
+            return
+        for i in range(self.row_end, self.row_start - 1, -1):
+            self.result.append(self.matrix[i][self.col_start])
+        self.col_start += 1
+        self.right()
 
+    def out_of_border(self):
+        return self.row_start > self.row_end or self.col_start > self.col_end
